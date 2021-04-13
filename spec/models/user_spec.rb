66b5fd6ecbbb53683a6f8df93eea0f89bfcfa6da
d-otis <dan@dan-foley.com>
2021-04-13 19:22:20 -0400

    create valid_attrs hash to use with validations testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  let(:valid_attrs) do 
+    {
+    :first_name => "Patsy",
+    :last_name => "Cline",
+    :email => "bluemoonkentucky@gmail.com",
+    :password => "crazy!1234%^&",
+    :password_confirmation => "crazy!1234%^&"
+    }
+  end
+
   context "create" do
+
+    let(:missing_first_name) { valid_attrs.except(:first_name) }
+    let(:missing_last_name) { valid_attrs.except(:last_name) }
+    let(:missing_email) { valid_attrs.except(:email) }
+    let(:missing_password) { valid_attrs.except(:password) }
+    let(:missing_password_confirmation) { valid_attrs.except(:password_confirmation) }
+    let(:password_mismatch) { valid_attrs.merge(:password_confirmation => '0000') }
+
     it "has a first_name attribute" do
       expect(User.new).to respond_to(:first_name)
     end
