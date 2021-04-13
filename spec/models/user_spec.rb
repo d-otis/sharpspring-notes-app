7ce1eaffc2d8855465a0ce00ec951797184a7165
d-otis <dan@dan-foley.com>
2021-04-13 18:59:00 -0400

    user spec for existence of attrs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "create" do
+    it "has a first_name attribute" do
+      expect(User.new).to respond_to(:first_name)
+    end
+    it "has a last_name attribute" do
+      expect(User.new).to respond_to(:last_name)
+    end
+    it "has a email attribute" do
+      expect(User.new).to respond_to(:email)
+    end
+    it "has a password attribute" do
+      expect(User.new).to respond_to(:password)
+    end
+    it "has a password_confirmation attribute" do
+      expect(User.new).to respond_to(:password_confirmation)
+    end
+  end
 end
