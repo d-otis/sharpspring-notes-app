880982288c714723bf7b53ffe532c3a38a9ae226
d-otis <dan@dan-foley.com>
2021-04-13 21:03:02 -0400

    spec attribute presence tests for Note

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:user) {
+    User.create(
+      :first_name => "Patsy",
+      :last_name => "Cline",
+      :email => "bluemoonkentucky@gmail.com",
+      :password => "crazy!1234%^&",
+      :password_confirmation => "crazy!1234%^&"
+    )
+  }
+
+  let(:valid_attrs) do
+    {
+      title: "First Note",
+      body: "Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+      pinned: true
+    }
+  end
+
+  context "attributes" do
+
+    it "has a title attribute" do
+      expect(Note.new).to respond_to(:title)
+    end
+
+    it "has a body attribute" do
+      expect(Note.new).to respond_to(:body)
+    end
+
+    it "has a pinned attribute" do
+      expect(Note.new).to respond_to(:pinned)
+    end
+
+    it "has a user_id attribute" do
+      expect(Note.new).to respond_to(:user_id)
+    end
+
+  end
 end
