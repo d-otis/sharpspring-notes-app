43848a79f5aff0d15f57eeac086180b4403012c0
d-otis <dan@dan-foley.com>
2021-04-14 11:26:16 -0400

    implement Note validations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,19 @@
 class Note < ApplicationRecord
   belongs_to :user, inverse_of: :notes
+
+  before_validation :check_title, unless: :title?
+
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :body, length: { maximum: 1000 }
+
+
+  private
+
+  def check_title
+    if body?
+      self.title = body.slice(0, 30)
+    else
+      :abort
+    end
+  end
 end
