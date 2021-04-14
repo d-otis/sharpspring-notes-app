e1e884ddb2e329f5e21ed5fc72820f0bea9e6bb5
d-otis <dan@dan-foley.com>
2021-04-13 20:21:21 -0400

    add User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
 end
