d2cd43571c5aeb37744a5ddfaf30e32686592ada
d-otis <dan@dan-foley.com>
2021-04-14 19:08:43 -0400

    add has_many through macros to Note model

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,6 +1,9 @@
 class Note < ApplicationRecord
   belongs_to :user, inverse_of: :notes
 
+  has_many :note_categories
+  has_many :categories, :through => :note_categories
+
   before_validation :check_title, unless: :title?
 
   validates :title, presence: true, length: { maximum: 30 }
