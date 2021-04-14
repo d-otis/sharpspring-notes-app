b6d3937a3f4aa40245d0ec72b5cefc2f65034b3a
d-otis <dan@dan-foley.com>
2021-04-14 16:22:06 -0400

    update schema with default pinned status

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_14_171238) do
+ActiveRecord::Schema.define(version: 2021_04_14_173953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -22,11 +22,18 @@ ActiveRecord::Schema.define(version: 2021_04_14_171238) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "note_categories", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.uuid "category_id"
+    t.uuid "note_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "notes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.uuid "user_id"
-    t.boolean "pinned"
+    t.boolean "pinned", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
