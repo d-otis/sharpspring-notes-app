a118cad10c2e16cfcf62f62fd940eceedbc90843
d-otis <dan@dan-foley.com>
2021-04-15 15:21:01 -0400

    add set_user before_action in UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,13 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  
   def show
   end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
+
 end
