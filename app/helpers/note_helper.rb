a060381247e7ccf542a56fa61aa3146b50bfb815
d-otis <dan@dan-foley.com>
2021-04-16 15:25:40 -0400

    add note_helper

---
+++ app/helpers/note_helper.rb
@@ -0,0 +1,15 @@
+module NoteHelper
+  def edit(note)
+    link_to("[edit]", edit_note_path(note))
+  end
+
+  def delete(note)
+    "[delete]"
+  end
+
+  def pin(note)
+    btn_text = note.pinned? ? "[unpin]" : "[pin]"
+
+    button_to btn_text, @user, method: :patch
+  end
+end
