153f77db14436df90a1eba10b263fe2986c1447c
d-otis <dan@dan-foley.com>
2021-04-16 16:07:52 -0400

    add route for /login

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get "/register", to: 'users#new'
+  get "/login", to: "sessions#new"
 
   resources :users
   resources :notes, except: :put
