92fd2cb4ed06889935766d4d00545fb39bfdfd7e
d-otis <dan@dan-foley.com>
2021-04-15 20:43:57 -0400

    define user CRUD actions in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,29 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to user_path(@user), notice: "You have created a new account!"
+    else
+      flash[:notice] = @user.errors.full_messages
+      render :new
+    end
+  end
+  
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    
   end
 
   private
