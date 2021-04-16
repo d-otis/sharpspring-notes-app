12b1759e735aabf3493bc3bc21ce73624b45296d
d-otis <dan@dan-foley.com>
2021-04-16 16:17:54 -0400

    flesh out sessions#create action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by(email: user_params[:email])
+
+    if @user && @user.authenticate(user_params[:password])
+      login_user(@user)
+
+      redirect_to user_path(@user)
+    else
+      flash[:notice] = "Error logging in" if @user.nil?
+      render :new
+    end
   end
 
   def destroy
