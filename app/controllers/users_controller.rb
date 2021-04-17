b8654b59226c97572baa6c68acc5b2371e21a976
d-otis <dan@dan-foley.com>
2021-04-17 11:47:28 -0400

    login user automatically after account creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      login_user(@user)
       redirect_to user_path(@user), notice: "You have created a new account!"
     else
       flash[:notice] = @user.errors.full_messages
