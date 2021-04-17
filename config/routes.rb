880c8d4cd1270cfd62eb09aeca2e23d4d7d29c93
d-otis <dan@dan-foley.com>
2021-04-17 11:52:43 -0400

    add logout route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get "/register", to: 'users#new'
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
+  get "/logout", to: "sessions#destroy"
 
   resources :users
   resources :notes, except: :put
