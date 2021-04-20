a93184809a79a8c5d1f0378977b539c1ba2ea136
d-otis <dan@dan-foley.com>
2021-04-20 10:29:33 -0400

    add GMAIL settings to config for Action Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,20 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "sharpspring_notes_app_production"
 
+
+  # ACTION MAILER 
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            ENV['GMAIL_USER'],
+    password:             ENV['GMAIL_PWD'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
